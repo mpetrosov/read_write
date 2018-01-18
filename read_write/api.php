6e88767dd2e9bf388e48036f8288eeecac3aa6ad
Marat Petrosov <34557212+mpetrosov@users.noreply.github.com>
2018-01-18 23:24:42 +0100

    update php api

--- read_write/api.php
+++ read_write/api.php
@@ -1,26 +1,56 @@
 <?php
 
+// allow cross domain requests
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, X-Request');
+
+// this script returns JSON
+header('Content-Type: application/json');
+
+// determine action, throw exception on error
 if (! isset($_GET['action'])) {
     throw new Exception("Url parameter not set: action");
-} else if ($_GET['action'] == 'read') {
+}
+
+$action = $_GET['action'];
+if ($action == 'read') {
     GetMessage($_GET['id']);
-} else if ($_GET['action'] == 'write') {
+} else if ($action == 'write') {
     WriteMessage($_GET['mykey'], $_GET['value']);
+} else if ($action == 'list') {
+    GetMessages($_GET['limit']);
 } else {
-    throw new Exception("Invalid url parameter value: " . $_GET['action']);
+    throw new Exception("Invalid action: " . $action);
+}
+
+function GetMessages($limit) {
+    $db = DbConnect();
+
+    $sql = "select * from messages order by id desc limit $limit";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+
+    $res = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // JSON_NUMERIC_CHECK converts numeric strings to numbers
+    // See: https://stackoverflow.com/questions/11128823/how-to-properly-format-pdo-results-numeric-results-returned-as-string
+    echo json_encode(array_reverse($res), JSON_NUMERIC_CHECK);
 }
 
 function GetMessage($id) {
     $db = DbConnect();
 
-    $sql = "select value from messages where id=:id";
+    $sql = "select * from messages where id=:id";
     $statement = $db->prepare($sql);
     $statement->execute(array(
         ':id' => $id
     ));
-    $row = $statement->fetch();
+    // fetch column name to value hash
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
 
-    echo $row['value'];
+    // JSON_NUMERIC_CHECK converts numeric strings to numbers
+    // See: https://stackoverflow.com/questions/11128823/how-to-properly-format-pdo-results-numeric-results-returned-as-string
+    echo json_encode($row, JSON_NUMERIC_CHECK);
 }
 
 function WriteMessage($mykey, $value) {
@@ -46,5 +76,3 @@ function DbConnect() {
 
     return $db;
 }
-
-?>
